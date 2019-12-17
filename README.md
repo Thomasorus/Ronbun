89add8e7d2773044ec2e5be0640114c946e33392
Thomasorus <contact@thomasorus.com>
2019-12-17 23:07:06 +0100

    Added basic file and functionnalities

---
+++ README.md
@@ -0,0 +1,8 @@
+# Wiki Engine
+
+This wiki engine needs to work this way:
+- The content is stored in markdown in subfolders who indicate the hierarchy and url
+- A script goes through all files, parses the markdown to html and populate a json file
+- the json is used to fill the website
+
+Basically the markdown files are useless once the json is generated. But
---
+++ database.js
@@ -0,0 +1,18 @@
+DATA.content = `
+{
+    "content": {
+        "home": {
+            "title": "Hello home",
+            "text": "lorem ipsum"
+        },
+        "test": {
+            "title": "Hello test",
+            "text": "lorem test"
+        },
+        "404": {
+            "title": "Page does not exist",
+            "text": "Please try again"
+        }
+    }
+}
+`
