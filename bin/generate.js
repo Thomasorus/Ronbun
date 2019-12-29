f2133f05e9032a9c4beec35129d0e10086b4ddd7
Thomasorus <contact@thomasorus.com>
2019-12-29 22:23:50 +0100

    Refactored generate script and included image and title

--- bin/generate.js
+++ bin/generate.js
@@ -42,45 +42,94 @@ function walk(dir) {
   });
 }
 
-function generateAll() {
-  walk("./content").then(data => {
-    let index = { content: {} };
+async function listFiles(folderPath) {
+  const data = await walk(folderPath);
+  console.log(
+    "Explored " + folderPath + " and found " + data.length + " entries."
+  );
+  return data;
+}
 
-    let pattern = /(.*\/)+(.*\.md)/;
-    console.log(`You have ${data.length} pages`);
-    data.forEach(p => {
-      let match = p.match(pattern);
-      if (!match || match.length < 1 || match[1] === "/") return;
+async function createEntries(index, data, imgList) {
+  console.log("Creating entries...");
+  let pattern = /(.*\/)+(.*\.md)/;
+  data.forEach(p => {
+    let match = p.match(pattern);
+    if (!match || match.length < 1 || match[1] === "/") return;
 
-      let subject = match[2].replace(/\//g, "").replace(".md", "");
-      let parent = match[1].replace("content", "").replace(/\//g, "");
-      parent === "" ? (parent = "root") : (parent = parent);
-      let textContent = fs.readFileSync(p, "utf8");
-      let parsed = reader.parse(textContent);
-      let result = writer.render(parsed);
-      let cleanedText = result.replace(/\n/g, "");
+    //Cleaning strings and define subject + parent
+    let subject = match[2].replace(/\//g, "").replace(".md", "");
+    let parent = match[1].replace("content", "").replace(/\//g, "");
+    if (parent === "") {
+      parent = "root";
+    } else {
+      parent = parent;
+    }
 
-      console.log(
-        "The subject is: " + subject + " and his parent is " + parent
-      );
+    //Get content
+    let textContent = fs.readFileSync(p, "utf8");
 
-      index.content[subject] = index.content[subject] || {};
-      index.content[subject].parent = parent || {};
-      index.content[subject].text = cleanedText || {};
-    });
+    //Set Title
+    var title = "Maze";
+    var tempTitle = textContent.match(".*#*.\n");
+    console.log(tempTitle);
+    if (tempTitle) {
+      let cleanedTitle = tempTitle[0].substr(2).replace(/\n/g, "");
+      console.log(cleanedTitle);
 
-    let jsonContent = JSON.stringify(index);
-    fs.writeFileSync(
-      "./database.js",
-      "DATA.content =`" + jsonContent.replace(/\\\"/g, "'") + "`",
-      err => {
-        if (err) {
-          console.log(err);
-          throw err;
-        }
+      title = cleanedTitle;
+    }
+
+    //Parsing markdown and converting to html
+    let parsed = reader.parse(textContent);
+    let result = writer.render(parsed);
+    let cleanedText = result.replace(/\n/g, "");
+
+    index.content[subject] = index.content[subject] || {};
+    index.content[subject].parent = parent || {};
+    index.content[subject].text = cleanedText || {};
+    index.content[subject].title = title || {};
+
+    //Define image name and url
+    imgList.forEach(obj => {
+      var testImg = obj.match(subject);
+      if (testImg) {
+        index.content[subject].img = obj || {};
+      } else {
+        index.content[subject].img = "media/cover.jpg" || {};
       }
+    });
+    console.log(
+      "Created " +
+        subject +
+        " with parent  " +
+        index.content[subject].parent +
+        " and image " +
+        index.content[subject].img
     );
   });
+}
+
+function generateDB(index) {
+  let jsonContent = JSON.stringify(index);
+  fs.writeFileSync(
+    "./database.js",
+    "DATA.content =`" + jsonContent.replace(/\\\"/g, "'") + "`",
+    err => {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+    }
+  );
+}
+
+async function generateAll() {
+  let index = { content: {} };
+  const imgList = await listFiles("./media");
+  const data = await listFiles("./content");
+  await createEntries(index, data, imgList);
+  generateDB(index);
 }
 
 generateAll();
