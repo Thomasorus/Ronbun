1a4ac80b4405ff025884cee12aa4a98e33975bcd
Thomasorus <contact@thomasorus.com>
2020-03-22 22:24:12 +0100

    Continued menus

--- bin/generate.js
+++ bin/generate.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const path = require("path");
-var rimraf = require("rimraf");
-const MarkdownIt = require("./markdown-it.min.js");
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+const MarkdownIt = require('./markdown-it.min.js');
+
 const md = new MarkdownIt({
-  typographer: false
+  typographer: false,
 });
 
 // https://gist.github.com/lovasoa/8691344#gistcomment-2631947
@@ -16,21 +17,22 @@ function walk(dir) {
         return reject(error);
       }
       Promise.all(
-        files.map(file => {
-          return new Promise((resolve, reject) => {
-            const filepath = path.join(dir, file);
-            fs.stat(filepath, (error, stats) => {
-              if (error) {
-                return reject(error);
-              }
-              if (stats.isDirectory()) {
-                walk(filepath).then(resolve);
-              } else if (stats.isFile()) {
-                resolve(filepath);
-              }
-            });
-          });
-        })
+        files.map(
+          file =>
+            new Promise((resolve, reject) => {
+              const filepath = path.join(dir, file);
+              fs.stat(filepath, (error, stats) => {
+                if (error) {
+                  return reject(error);
+                }
+                if (stats.isDirectory()) {
+                  walk(filepath).then(resolve);
+                } else if (stats.isFile()) {
+                  resolve(filepath);
+                }
+              });
+            })
+        )
       ).then(foldersContents => {
         resolve(
           foldersContents.reduce(
@@ -43,65 +45,59 @@ function walk(dir) {
   });
 }
 
+// Listing all files
 async function listFiles(folderPath) {
   const data = await walk(folderPath);
-  console.log(
-    "Explored " + folderPath + " and found " + data.length + " entries."
-  );
+  console.log(`Explored ${folderPath} and found ${data.length} entries.`);
   return data;
 }
 
-async function createEntries(index, navigation, data, imgList) {
-  console.log("Creating entries...");
-  let filePattern = /(.*\/)+(.*\.md)/;
+async function createEntries(index, data, imgList) {
+  console.log('Creating entries...');
+  const filePattern = /(.*\/)+(.*\.md)/;
 
   data.forEach(p => {
-    let match = p.match(filePattern);
-    if (!match || match.length < 1 || match[1] === "/") return;
+    // Checking for the right file pattern
+    const match = p.match(filePattern);
+    if (!match || match.length < 1 || match[1] === '/') return;
 
-    //Cleaning strings and define subject + parent
-
-    let subject = match[2].replace(/\//g, "").replace(".md", "");
-    let parents = match[1]
-      .replace("content/", "")
+    // Cleaning strings and defining subject + parent
+    const subject = match[2].replace(/\//g, '').replace('.md', '');
+    const parents = match[1]
+      .replace('content/', '')
       .slice(0, -1)
-      .split("/");
+      .split('/');
     let parent = parents[parents.length - 1];
 
-    if (parent === "" || parent === subject) {
-      parent = "home";
-    } else if (parent === "home") {
+    if (parent === '' || parent === subject) {
+      parent = 'index';
+    } else if (parent === 'index') {
       parent = null;
     }
 
-    //Get content
-    let textContent = fs.readFileSync(p, "utf8");
-    let noTitleContent = textContent.replace(/.*#.*\n\n/m, "");
+    // Get content
+    const textContent = fs.readFileSync(p, 'utf8');
 
-    //Set Title
-    var title = "Maze";
-    var tempTitle = textContent.match(".*#*.\n");
+    // Set Title
+    let title = 'Maze';
+    const tempTitle = textContent.match('.*#*.\n');
     if (tempTitle) {
-      let cleanedTitle = tempTitle[0].substr(2).replace(/\n/g, "");
+      const cleanedTitle = tempTitle[0].substr(2).replace(/\n/g, '');
       title = cleanedTitle;
     }
 
-    //Parsing markdown and converting to html
-    let result = md.render(textContent);
-    let cleanedText = result.replace(/\n/g, "");
+    // Parsing markdown and converting to html
+    const result = md.render(textContent);
+    const cleanedText = result.replace(/\n/g, '');
 
     index.content[subject] = index.content[subject] || {};
     index.content[subject].parent = parent || {};
-    index.content[subject].text = cleanedText || {};
+    index.content[subject].text = 'cleanedText' || {};
     index.content[subject].title = title || {};
-    // index.content[subject].url = url || {};
 
-    // navigation.menu[parent] = navigation.menu[parent] || {};
-    // navigation.menu[subject].parents = parents || {};
-
-    //Define image name and url
+    // Define image name and url
     imgList.forEach(obj => {
-      var testImg = obj.match(subject);
+      const testImg = obj.match(subject);
       if (testImg) {
         index.content[subject].img = obj || {};
       }
@@ -109,39 +105,86 @@ async function createEntries(index, navigation, data, imgList) {
   });
 }
 
-function generateDB(index, navigation) {
-  let menusContent = JSON.stringify(navigation);
-  fs.writeFileSync(
-    "./navigation.json",
-    menusContent.replace(/\\\"/g, "'"),
-    err => {
-      if (err) {
-        console.log(err);
-        throw err;
-      }
-    }
-  );
+function templatingPage(title, body, parent, siblings, mainMenu) {
+  const pageTitle = title || '';
+  const pageBody = body || '';
+  // const pageMenus = menus || '';
+  const pageParent = parent || '';
+  const pageMenuSiblings = siblings || '';
+  // const pageHeader = header || '';
+  // const pageFooter = footer || '';
+  const template = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="./style.css" />
+    <title>${pageTitle}</title>
+  </head>
+  <body>
+    <header></header>
 
-  let jsonContent = JSON.stringify(index);
-  fs.writeFileSync(
-    "./database.js",
-    "DATA.content =`" + jsonContent.replace(/\\\"/g, "'") + "`",
-    err => {
-      if (err) {
-        console.log(err);
-        throw err;
-      }
+    <main>
+      <div>${mainMenu}</div>
+      <article>
+        ${pageBody}
+      </article>
+      <aside>
+        <nav>
+          ${pageMenuSiblings}
+        </nav>
+      </aside>
+    </main>
+  </body>
+</html>
+  `;
+  return template;
+}
+
+function createMenus(content, key, parent) {
+  let items = '';
+  Object.keys(content).forEach(function(i) {
+    if (content[i].parent === parent && i !== parent) {
+      const link = `
+        <li>
+        <a class="${i === key ? `active` : ''}" href="${i}.html">
+        ${content[i].title}
+        </a>
+      </li>`;
+      items += link;
     }
-  );
+  });
+  const mainMenu = '<h3>Select a subject</h3>';
+  const siblingsMenu = `
+    <h3>This page is part of 
+      ${parent.replace(/-/g, ' ')}
+    </h3>
+  `;
+  const introMenu = parent === key ? mainMenu : siblingsMenu;
+  const menus = `
+      ${introMenu}
+      <ul>${items}</ul>
+    `;
+  return menus;
 }
 
 async function generateHtml(index) {
-  const content = index.content;
+  const { content } = index;
 
   for (const key of Object.keys(content)) {
-    let obj = content[key];
-    let menus = createMenu(content, key, obj.parent);
-    const body = templatingPage(obj.title, obj.text, menus);
+    const obj = content[key];
+    const siblings = createMenus(content, key, obj.parent);
+    const mainMenu = createMenus(content, 'index', 'index');
+
+    const body = templatingPage(
+      obj.title,
+      obj.text,
+      obj.parent,
+      siblings,
+      mainMenu
+    );
 
     fs.writeFileSync(`./dist/${key}.html`, body, err => {
       if (err) {
@@ -152,41 +195,24 @@ async function generateHtml(index) {
   }
 }
 
-function templatingPage(title, body, menus, header, footer) {
-  let pageTitle = title ? title : "";
-  let pageBody = body ? body : "";
-  let pageMenus = menus ? menus : "";
-  let pageHeader = header ? pageHeader : "";
-  let pageFooter = footer ? pageFooter : "";
-  let template = `<!DOCTYPEhtml><htmllang="en"><head><metacharset="UTF-8"/><metaname="viewport"content="width=device-width,initial-scale=1.0"/><metahttp-equiv="X-UA-Compatible"content="ie=edge"/><title>${pageTitle}</title><style>html{font-size:10px;}body{margin:0;padding:0;font-family:-apple-system,BlinkMacSystemFont,"SegoeUI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"HelveticaNeue",sans-serif;font-variant-numeric:oldstyle-nums;font-size:1.6rem;background-color:whitesmoke;}img{max-width:100%;}main{max-width:600px;margin:auto;}.active{color:red;}</style></head><body>${pageHeader}${pageMenus}${pageBody}${pageFooter}</body></html>`;
-
-  return template;
+async function getCss(cssPath, cssDestination) {
+  fs.copyFile(cssPath, cssDestination, err => {
+    if (err) throw err;
+  });
 }
-
-//Create menu from pages
-function createMenu(content, key, parent) {
-  let listItems = "";
-  for (const i of Object.keys(content)) {
-    let link = `
-    <li><a class="${i === key ? `active` : ""}" href="${i}.html">${i}</a></li>`;
-    listItems += link;
-  }
-  const menus = "<ul>" + listItems + "</ul>";
-  return menus;
-}
-
-function generatePages(index) {}
 
 async function generateAll() {
-  rimraf.sync("./dist");
-  fs.mkdirSync("./dist");
-  let index = { content: {} };
-  let navigation = { menu: {} };
-  const imgList = await listFiles("./media");
-  const data = await listFiles("./content");
-  await createEntries(index, navigation, data, imgList);
-  await generateHtml(index);
-  // generateDB(index, navigation);
+  rimraf.sync('./dist');
+  fs.mkdirSync('./dist');
+  const index = { content: {} };
+  const imgList = await listFiles('./media');
+  const data = await listFiles('./content');
+  await createEntries(index, data, imgList);
+  console.log(index);
+  // await generateHtml(index);
+  // await getCss('./style.css', './dist/style.css');
+
+  // Todo : move all files into dist/media and compress them
 }
 
 generateAll();
