3528e6ae2f95ebe50e20f1b3a50e2b15ff4958fb
Thomasorus <contact@thomasorus.com>
2020-11-17 15:16:24 +0100

    Added config file with paths

---
+++ src/config.mjs
@@ -0,0 +1,18 @@
+export const config = {
+    srcDataDir : "./data",
+    srcAssetsDir : "./assets",
+    srcMediaDir : "./media",
+
+    buildDir : "./www",
+    buildAssetsDir : "./www/assets",
+    buildMediaDir : "./www/media",
+
+    contentFile: "content.kaku",
+    timeFile: "time.kaku",
+
+    htmlTemplate: "main.html",
+    rssTemplate: "rss.xml",
+    itemTemplate:"item.xml",
+
+    contentSplitter: "===="
+}
