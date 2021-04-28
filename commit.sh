e13ba77a57477e798e6d7c5e8d0f72445eb5255e
Thomasorus <contact@thomasorus.com>
2021-04-28 21:55:22 +0200

    *

--- commit.sh
+++ commit.sh
@@ -1,1 +1,1 @@
-cd www && git add --all && git commit -m "*" && git push origin master && cd ../data && git add --all && git commit -m "*" && git push origin master && cd ../ && git add --all && git commit -m "*" && git push origin master
+cd ./www && git add --all && git commit -m "*" && git push origin master && cd ./../data && git add --all && git commit -m "*" && git push origin master && cd ./../ && git add --all && git commit -m "*" && git push origin master
