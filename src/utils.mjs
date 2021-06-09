21bb772d9e40d134228da74a7e264aba9da380d2
Thomasorus <contact@thomasorus.com>
2021-06-10 00:56:04 +0200

    Started page generation and added assets copy

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,12 +1,13 @@
 import * as fs from 'fs'
+import {execSync} from 'child_process'
+
 
 export function mkDir (dir) {
   if (fs.existsSync(dir)) {
-    console.log(`The ${dir} directory exists!`)
+    console.log(` Skipping ${dir} directory`)
   } else {
-    console.log(`The ${dir} directory was not found. Creating...`)
+    console.log(`+ Creating ${dir} directory`)
     fs.mkdirSync(dir)
-    console.log('Done')
   }
 }
 
@@ -24,44 +25,6 @@ export function readFile (path) {
   return file
 }
 
-export async function copyAsset (buildAssetsDir, srcAssetsDir, file) {
-  if (fs.existsSync(`${buildAssetsDir}/${file}`)) {
-    const oldFile = await getFileSize(`${buildAssetsDir}/${file}`)
-    const newFile = await getFileSize(`${srcAssetsDir}/${file}`)
-    if (oldFile !== newFile) {
-      console.log(`Changes for ${file}`)
-      fs.copyFile(`${srcAssetsDir}/${file}`, `${buildAssetsDir}/${file}`, err => {
-        if (err) {
-          throw err
-        }
-      })
-      return true
-    } else {
-      console.log(`No change for ${file}`)
-      return false
-    }
-  } else {
-    console.log(`Copy of ${file}`)
-    fs.copyFile(`${srcAssetsDir}/${file}`, `${buildAssetsDir}/${file}`, err => {
-      if (err) {
-        throw err
-      }
-    })
-    return true
-  }
-}
-
-// Returns the file size
-export async function getFileSize (file) {
-  const stats = fs.statSync(file)
-  return stats.size
-}
-
-// Splits a text file into an array of objects based on a string
-export async function splitContent (textContent, splitter) {
-  const splitContent = textContent.split(splitter)
-  return splitContent
-}
 
 export async function compareStrings (oldPage, item, reg) {
   const regex = RegExp(reg)
@@ -95,4 +58,34 @@ export async function toIsoDate (date) {
   const isoDate = `${day}T${fixedHour.join(':')}Z`
   const formatedDate = new Date(isoDate)
   return formatedDate
+}
+
+
+export async function moveAssets(sourceDir, destinationDir) {
+    const dir = await fs.promises.opendir(sourceDir)
+    for await (const file of dir) {
+      if(!fs.existsSync(`${destinationDir}/${file.name}`)) {
+        console.log(`Copying new asset ${file.name}`)
+       fs.copyFile(`${sourceDir}/${file.name}`, `${destinationDir}/${file.name}`, err => {
+          if (err) {
+            throw err
+          }
+        })
+      } else {
+        const destSize = fs.statSync(`${destinationDir}/${file.name}`).size.toString().trim()
+        const sourceSize = fs.statSync(`${sourceDir}/${file.name}`).size.toString().trim()
+        // const gitSize = await execSync(`git ls-tree -r  -l HEAD ${sourceDir}/${file.name}`).toString().replace(`${sourceDir}/${file.name}`.substring(2), '').substring(52).trim()
+
+        if(sourceSize !== destSize) {
+          console.log(`Updating ${file.name.trim()}`)
+          fs.copyFile(`${sourceDir}/${file.name}`, `${destinationDir}/${file.name}`, err => {
+            if (err) {
+              throw err
+            }
+          })
+        } else {
+          console.log(` Skipping ${file.name}`)
+        }
+      }
+    }
 }
