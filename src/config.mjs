4dab97b33b2ce23259499eb1446f66adb65b7c24
Thomasorus <contact@thomasorus.com>
2021-11-06 18:22:14 +0100

    Simplified config file

--- src/config.mjs
+++ src/config.mjs
@@ -1,18 +1,13 @@
 export const config = {
-  srcDataDir: './data/content',
-  srcAssetsDir: './assets',
-  srcMediaDir: './data/media',
-
   buildDir: './www',
   buildAssetsDir: './www/assets',
   buildMediaDir: './www/media',
-
-  contentFile: 'content.kaku',
-  timeFile: 'time.kaku',
 
   htmlTemplate: 'main.html',
   rssTemplate: 'rss.xml',
   itemTemplate: 'item.xml',
 
-  contentSplitter: '===='
+  contentSplitter: '====',
+  content: "./data/content/content.kaku",
+  time: "./data/content/time.kaku"
 }
