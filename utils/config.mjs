191231e1902a8e735bbdfb27d0fb46b28d653996
Thomasorus <contact@thomasorus.com>
2022-06-30 14:09:58 +0200

    Redid structure

---
+++ utils/config.mjs
@@ -0,0 +1,16 @@
+export const config = {
+    buildDir: './www',
+    buildAssetsDir: './www/assets',
+    buildMediaDir: './www/media',
+
+    srcAssetsDir: './assets',
+
+    htmlTemplate: './assets/main.html',
+    rssTemplate: './assets/rss.xml',
+    itemTemplate: './assets/item.xml',
+
+    contentSplitter: '====',
+    content: "./data/content/content.kaku",
+    time: "./data/content/time.kaku",
+    feed: "./www/feed.xml"
+}
