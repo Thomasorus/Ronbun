d97840d9e155c12217ccc13d77ed6a6742cd3543
Thomasorus <contact@thomasorus.com>
2023-03-05 23:39:12 +0100

    Cleaning

--- pages.tmpl.js
+++ pages.tmpl.js
@@ -1,12 +1,5 @@
 export default function* ({ allContent }) {
   for (const node of dfs(allContent[0])) {
-  //   if(node.created) {
-
-  //   console.log(node.name)
-  //   console.log(node.created)
-  //   console.log(node.date)
-  //   console.log("")
-  // }
     yield {
       layout: "layout.njk",
       created: node.created,
