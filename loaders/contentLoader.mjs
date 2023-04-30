e33317bc1ef8e1e2d94ff530e789e8a59d36569a
Thomasorus <contact@thomasorus.com>
2023-04-30 15:10:41 +0200

    Fixed the comparaison

--- loaders/contentLoader.mjs
+++ loaders/contentLoader.mjs
@@ -216,7 +216,7 @@ export async function contentLoader() {
 function checkDate(page, feedText) {
   let date = undefined
   feedText.forEach((el) => {
-    const titleP = `<title>(${page.name}) - Thomasorus</title>`;
+    const titleP = `<title>(${page.name})</title>`;
     const re = new RegExp(titleP, "");
     const rssTest = re.test(el);
     const currentDate = /<updated>(.*)<\/updated>/.exec(el)
