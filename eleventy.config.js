2a4ab447903c05d4c5bad250da6557e7e74b0764
Thomasorus <contact@thomasorus.com>
2024-10-19 13:37:09 +0200

    Added html minification

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,10 @@
-import {DateTime} from "luxon"
+import { DateTime } from "luxon";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import lightningCSS from "@11tyrocks/eleventy-plugin-lightningcss";
 import pluginRss from "@11ty/eleventy-plugin-rss";
+import htmlmin from "html-minifier-terser";
 
-export default async function(eleventyConfig) {
+export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "assets/fonts/subsetted": "fonts" });
   eleventyConfig.addPassthroughCopy({ "assets/index.js": "index.js" });
   eleventyConfig.addPassthroughCopy({ "assets/*.svg": "assets/" });
@@ -11,23 +12,36 @@ export default async function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "assets/*.png": "assets/" });
   eleventyConfig.addPassthroughCopy({ "img/": "img/" });
 
-
   eleventyConfig.addFilter("date", (dateObj) => {
-      return DateTime.fromISO(dateObj).toLocaleString(DateTime.DATE_MED);
+    return DateTime.fromISO(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
- 	eleventyConfig.addFilter("dateToRfc3339", pluginRss.dateToRfc3339);
+  eleventyConfig.addFilter("dateToRfc3339", pluginRss.dateToRfc3339);
 
   eleventyConfig.addPlugin(lightningCSS);
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
-		extensions: "html",
-		formats: ["avif", "webp", "jpg"],
+    extensions: "html",
+    formats: ["avif", "webp", "jpg"],
     widths: [700],
-		defaultAttributes: {
-			loading: "lazy",
-			decoding: "async",
-			sizes: "700px"
-		},
-	});
+    defaultAttributes: {
+      loading: "lazy",
+      decoding: "async",
+      sizes: "700px",
+    },
+  });
 
-};
+  eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+    if (outputPath.endsWith(".html")) {
+      return htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        continueOnParseError: true,
+        removeAttributeQuotes: true,
+        minifyCSS: true,
+      });
+    }
+
+    return content;
+  });
+}
